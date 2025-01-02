32730bf9b35b75552a3e417ddb418dfa6fd2acb5
aisudo19 <indigonaka@gmail.com>
2025-01-03 02:55:59 +0900

    食材リストのCSSを追加

--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom';
+import styles from '../assets/css/FoodList.module.css';
 
 function FoodList() {
   const location = useLocation();
   const { foodListString } = location.state || { foodListString: '' };
   const formattedText = foodListString.split('\n').map((line, index) => (
-    <p key={index}>{line}</p>
+    <li key={index}>{line}</li>
   ));
 
   return (
     <div>
       <h1>食材リスト</h1>
+      <ul>
         {formattedText}
+      </ul>
     </div>
   )
 }
