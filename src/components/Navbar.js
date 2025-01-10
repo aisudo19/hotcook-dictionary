39bfedfc61a61b1184f5369930ebeb28b7d1b0ce
aisudo19 <indigonaka@gmail.com>
2025-01-10 10:41:46 +0900

    ナビゲーションのスタイル修正

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,12 +12,20 @@ function Navbar() {
       <Link to='/'><FontAwesomeIcon icon={faHome} />ホーム</Link>
       <Link to='/saved-meal-plan-lists'><FontAwesomeIcon icon={faUtensils} />献立一覧</Link>
       {user ? (
-        <div>
-          <Link to='/logout'><FontAwesomeIcon icon={faRightFromBracket} />ログアウト</Link>
-          <Link to='/bulk_upload'><FontAwesomeIcon icon={faUpload} />データ一括Upload</Link>
-          <Link to='/new_recipe'>新しいレシピを追加</Link>
-          <Link to='/profile'><FontAwesomeIcon icon={faUser} />{user.displayName}</Link>
-        </div>
+        <>
+          <div>
+            <Link to='/logout'><FontAwesomeIcon icon={faRightFromBracket} />ログアウト</Link>
+          </div>
+          <div>
+            <Link to='/bulk_upload'><FontAwesomeIcon icon={faUpload} />データ一括Upload</Link>
+          </div>
+          <div>
+            <Link to='/new_recipe'>新しいレシピを追加</Link>
+          </div>
+          <div>
+            <Link to='/profile'><FontAwesomeIcon icon={faUser} />{user.displayName}</Link>
+          </div>
+        </>
       ) : (
         <Link to='/login'><FontAwesomeIcon icon={faRightToBracket} />ログイン</Link>
       )}
